31f9a47469eff0a4440591a6c95388353a634e7d
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-08-09 23:12:12 -0300

    Front end - initial project

---
+++ app/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google';
+
+import Dashboard from '../components/Dashboard';
+import NavBar from '../components/NavBar';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="flex h-screen bg-gray-200">
+      <NavBar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
+  );
+}
