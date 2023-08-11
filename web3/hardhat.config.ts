beff91fb1e25f7b3de5c9f18650b9a2528236d71
Lorenzo Alencar Tomaz <lolo.tomaz@gmail.com>
2023-08-11 15:24:25 -0300

    added scripts for deployment

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -8,27 +8,38 @@ const config: HardhatUserConfig = {
     version: "0.8.17",
   },
   networks: {
+    hardhat: {
+      forking: {
+        url: "https://goerli.base.org",
+        blockNumber: 0,
+      },
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
     local: {
       url: "http://localhost:8545",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
     },
-    // for mainnet
-    // "base-mainnet": {
-    //   url: "https://mainnet.base.org",
-    //   accounts: [process.env.WALLET_KEY as string],
-    //   gasPrice: 1000000000,
-    // },
-    // // for testnet
-    // "base-goerli": {
-    //   url: "https://goerli.base.org",
-    //   accounts: [process.env.WALLET_KEY as string],
-    //   gasPrice: 1000000000,
-    // },
-    // // for local dev environment
-    // "base-local": {
-    //   url: "http://localhost:8545",
-    //   accounts: [process.env.WALLET_KEY as string],
-    //   gasPrice: 1000000000,
-    // },
+    //for mainnet
+    "base-mainnet": {
+      url: "https://mainnet.base.org",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    "base-goerli": {
+      url: "https://goerli.base.org",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    "base-local": {
+      url: "http://localhost:8545",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
   },
   defaultNetwork: "hardhat",
 };
