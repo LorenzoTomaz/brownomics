584b4609748c789c51ec9f8c492e34afed2bbee6
Glaicon <gjpeixer@gmail.com>
2023-08-10 23:46:37 -0300

    remove tons of unnecessary code

--- api/server/models/simulation.py
+++ api/server/models/simulation.py
@@ -1,28 +1,33 @@
 from typing import Optional
 
-from pydantic import UUID5, BaseModel, EmailStr, Field
+from pydantic import UUID5, BaseModel, EmailStr, Field, PositiveFloat
 
 
-class SimulationSchema(BaseModel):
-    wallet: str = Field(...)
+class SimulationControlSchema(BaseModel):
+    initial_state: float = Field(100.0)
+    time_horizon: int = Field(365)
+    p_tok_t: float = Field(10)
+    s_t: int = Field(1000000)
+    gamma: float = Field(0.9)
+    expected_future_price: float = Field(11)
+    initial_population: int = Field(100)
+    adoption_rate: float = Field(0.1)
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "wallet": "John Doe"
-            }
-        }
+class SimulationBrownSchema(BaseModel):
+    period: int = Field(365)
+    agents: int = Field(1000)
+    times: int = Field(1)
+    beta: float = Field(0.3)
+    chi: float = Field(1)
+    interest_rate: float = Field(0.05)
+    token_supply: float = Field(1000000000)
+    price_mu: float = Field(0.03)
+    productivity_initial_value: float = Field(100)
+    productivity_mu: float = Field(0.02)
+    productivity_sigma: float = Field(2)
+    utility_mu: float = Field(1)
+    utility_sigma: float = Field(10)
 
-
-class UpdateSimulationModel(BaseModel):
-    wallet: str = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "wallet": "John Doe"
-            }
-        }
 
 
 def ResponseModel(data, message):
