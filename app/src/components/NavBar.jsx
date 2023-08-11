2634d396f3023141dbb7ee9cc9ebd6290adf799e
Lorenzo Alencar Tomaz <lolo.tomaz@gmail.com>
2023-08-11 15:30:29 -0300

    fixed linter issues

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -1,46 +1,116 @@
-import { useState } from 'react';
+import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
-
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true);
 
   return (
-    <div className={`fixed z-30 inset-y-0 left-0 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 ${show ? 'w-72' : 'w-12'}`}>
-      <div className='relative h-10'>
-        <button type="button" className="absolute top-2 right-2 p-2 inline-flex items-center justify-center text-gray-400" onClick={() => setShow(!show)
-        }>
-          {show && <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>}
-          {!show && <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
-          </svg>}
+    <div
+      className={`fixed z-30 inset-y-0 left-0 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 ${
+        show ? "w-72" : "w-12"
+      }`}
+    >
+      <div className="relative h-10">
+        <button
+          type="button"
+          className="absolute top-2 right-2 p-2 inline-flex items-center justify-center text-gray-400"
+          onClick={() => setShow(!show)}
+        >
+          {show && (
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+          {!show && (
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 8 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
+              />
+            </svg>
+          )}
         </button>
       </div>
-      {show && <>
-        <div className="flex items-center justify-center mt-8">
-          <div className="flex items-center">
-            <svg className="h-12 w-12" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z" fill="#4C51BF" stroke="#4C51BF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-              <path d="M201.694 387.105C231.686 417.098 280.312 417.098 310.305 387.105C325.301 372.109 332.8 352.456 332.8 332.8C332.8 313.144 325.301 293.491 310.305 278.495C295.309 263.498 288 256 275.2 230.4C256 243.2 243.201 320 243.201 345.6C201.694 345.6 179.2 332.8 179.2 332.8C179.2 352.456 186.698 372.109 201.694 387.105Z" fill="white"></path>
-            </svg>
-            <span className="text-white text-2xl mx-2 font-semibold">BROWNOMICS</span>
+      {show && (
+        <>
+          <div className="flex items-center justify-center mt-8">
+            <div className="flex items-center">
+              <svg
+                className="h-12 w-12"
+                viewBox="0 0 512 512"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z"
+                  fill="#4C51BF"
+                  stroke="#4C51BF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+                <path
+                  d="M201.694 387.105C231.686 417.098 280.312 417.098 310.305 387.105C325.301 372.109 332.8 352.456 332.8 332.8C332.8 313.144 325.301 293.491 310.305 278.495C295.309 263.498 288 256 275.2 230.4C256 243.2 243.201 320 243.201 345.6C201.694 345.6 179.2 332.8 179.2 332.8C179.2 352.456 186.698 372.109 201.694 387.105Z"
+                  fill="white"
+                ></path>
+              </svg>
+              <span className="text-white text-2xl mx-2 font-semibold">
+                BROWNOMICS
+              </span>
+            </div>
           </div>
-        </div>
-        <nav className="mt-10">
-          <a className="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100" href="/">
-            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
-            </svg>
+          <nav className="mt-10">
+            <Link
+              className="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100"
+              href="/"
+            >
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+                ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+                ></path>
+              </svg>
 
-            <span className="mx-3">Simulator parameters</span>
-          </a>
-        </nav>
-      </>}
+              <span className="mx-3">Simulator parameters</span>
+            </Link>
+          </nav>
+        </>
+      )}
     </div>
-  )
+  );
 }
