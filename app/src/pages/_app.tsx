3b1411b436c23f46f309d8b2cd1e04c8e27e2e0e
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2023-08-12 17:17:20 -0300

    new Forn

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,17 +1,26 @@
-import "@/styles/globals.css";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import '@radix-ui/themes/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+
+import { Theme } from '@radix-ui/themes';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThirdwebProvider
-        activeChain="base-goerli"
-        clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      >
-        <title>BROWNOMICS</title>
-        <Component {...pageProps} />
-      </ThirdwebProvider>
+      <Theme>
+        <ThirdwebProvider
+          activeChain="base-goerli"
+          clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+        >
+          <title>BROWNOMICS</title>
+          <Component {...pageProps} />
+        </ThirdwebProvider>
+        <ToastContainer />
+      </Theme>
     </>
   );
 }
